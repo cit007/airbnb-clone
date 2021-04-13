84114e882662ac24f2085718786f42251acb6215
cit007 <itcho0077@gmail.com>
2021-04-13 20:37:31 +0900

    user admin with admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 # Register your models here.
 # https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Custom User Admin """
 
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = ("language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
