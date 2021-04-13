d511a045de4d7e21c5fdbabbff16e9230394159b
cit007 <itcho0077@gmail.com>
2021-04-13 19:26:54 +0900

    custom user model and set some field with choice option(drop menu from admin panel)

--- users/models.py
+++ users/models.py
@@ -4,4 +4,38 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANG_EN = "en"
+    LANG_JP = "jp"
+    LANG_CHOICES = (
+        (LANG_EN, "English"),
+        (LANG_JP, "Japanese"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_JPY = "yen"
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_JPY, "YEN"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthday = models.DateField(null=True)
+    language = models.CharField(choices=LANG_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
