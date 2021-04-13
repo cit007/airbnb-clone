28f2c029b2da3b72abadeb87c9562d46759472a9
cit007 <itcho0077@gmail.com>
2021-04-13 21:01:29 +0900

    RECAP

--- users/models.py
+++ users/models.py
@@ -30,12 +30,10 @@ class User(AbstractUser):
         (CURRENCY_JPY, "YEN"),
     )
 
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
-    )
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     bio = models.TextField(default="", blank=True)
-    birthday = models.DateField(null=True)
+    birthday = models.DateField(blank=True, null=True)
     language = models.CharField(choices=LANG_CHOICES, max_length=2, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
